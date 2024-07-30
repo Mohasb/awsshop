aac15fd520e4c8edab7aed8da364b7aa317fa566
Muhammad Hicho Haidor <mh.haidor@gmail.com>
2024-07-30 15:56:15 +0200

    landing workin slider down and product 100 heigth

--- lib/components/landing/landing.dart
+++ lib/components/landing/landing.dart
@@ -1,62 +1,9 @@
-/* import 'package:flutter/material.dart';
-import 'package:awsshop/models/product.dart';
-import 'package:awsshop/components/landing/landing_product.dart';
-
-class Landing extends StatefulWidget {
-  const Landing({super.key});
-
-  @override
-  _LandingState createState() => _LandingState();
-}
-
-class _LandingState extends State<Landing> {
-  // Simulación de productos con múltiples imágenes
-  final List<Product> products = [
-    Product(
-      id: 1,
-      name: 'iPhone 13 Pro',
-      price: 1299.99,
-      description: 'El último iPhone con gran cámara y pantalla OLED.',
-      imageList: [
-        'https://m.media-amazon.com/images/I/71iIaQjJunL._AC_UF894,1000_QL80_.jpg',
-        'https://thumb.pccomponentes.com/w-530-530/articles/57/578926/1959-apple-iphone-13-pro-128gb-grafito-libre.jpg',
-        'https://media.ldlc.com/r1600/ld/products/00/05/88/59/LD0005885979_1_0005927925.jpg',
-        'https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202407/09/00194612400294____15__640x640.jpg',
-      ],
-    ),
-    Product(
-      id: 2,
-      name: 'Samsung Galaxy S21',
-      price: 999.99,
-      description: 'Potente smartphone con cámara de alta resolución.',
-      imageList: [
-        'https://via.placeholder.com/800x400.png?text=Samsung+Galaxy+S21',
-        'https://via.placeholder.com/800x400.png?text=Galaxy+S21+Front',
-      ],
-    ),
-    // Añadir más productos si es necesario
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ProductItemLanding(product: products[index]);
-        },
-    );
-  }
-}
- */
-
-import 'package:awsshop/components/landing/landing_product.dart';
-import 'package:awsshop/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:awsshop/components/landing/landing_product.dart';
+import 'package:awsshop/models/product.dart';
 
 class Landing extends StatelessWidget {
-  /* final List<Product> products; */
-
-  const Landing({super.key /* , required this.products */});
+  const Landing({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -84,17 +31,27 @@ class Landing extends StatelessWidget {
           'https://images.samsung.com/is/image/samsung/p6pim/es/sm-a057gzkueub/gallery/es-galaxy-a05s-sm-a057-sm-a057gzkueub-539177447?\$650_519_PNG\$',
         ],
       ),
-      // Añadir más productos si es necesario
+      // Agregar más productos si es necesario
     ];
 
-    return ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: ProductItemLanding(product: products[index]),
-          );
-        },
-      );
+    return FullScreenSlider(
+      pages: products.map((product) => ProductItemLanding(product: product)).toList(),
+    );
+  }
+}
+
+class FullScreenSlider extends StatelessWidget {
+  final List<Widget> pages;
+
+  const FullScreenSlider({super.key, required this.pages});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: pages,
+      ),
+    );
   }
 }
--- lib/components/landing/landing_product.dart
+++ lib/components/landing/landing_product.dart
@@ -1,89 +1,4 @@
-/* import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:awsshop/models/product.dart';
-
-class ProductItemLanding extends StatelessWidget {
-  final Product product;
-
-  const ProductItemLanding({super.key, required this.product});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            // Carrusel de imágenes
-            Expanded(
-              flex: 9,
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: double.infinity,
-                  aspectRatio: 16 / 9,
-                  viewportFraction: 1.0,
-                  enlargeCenterPage: false,
-                  autoPlay: true,
-                  autoPlayCurve: Curves.easeInOut,
-                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                ),
-                items: product.imageList
-                        ?.map((imageUrl) => _buildImageItem(imageUrl))
-                        .toList() ??
-                    [],
-              ),
-            ),
-            // Información del producto
-            Expanded(
-              flex: 1,
-              child: Container(
-                width: double.infinity,
-                color: Colors.grey.shade200,
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        product.name ?? 'Nombre del producto',
-                        style: const TextStyle(fontSize: 16),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        product.price != null
-                            ? '\$${product.price!.toStringAsFixed(2)}'
-                            : 'Sin precio',
-                        style: const TextStyle(fontSize: 16),
-                        textAlign: TextAlign.end,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildImageItem(String imageUrl) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
-}
- */
-
-/* import 'package:awsshop/models/product.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -94,33 +9,35 @@ class ProductItemLanding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          // Carrusel de imágenes que ocupa el 90% de la altura
-          Expanded(
-            flex: 9,
-            child: CarouselSlider(
-              options: CarouselOptions(
-                height: double.infinity,
-                aspectRatio: 16 / 9,
-                viewportFraction: 1.0,
-                enlargeCenterPage: false,
-                autoPlay: true,
-                autoPlayCurve: Curves.easeInOut,
-                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Usar LayoutBuilder para obtener el tamaño del contenedor padre
+        double containerHeight = constraints.maxHeight;
+
+        return Column(
+          children: [
+            // Carrusel de imágenes que ocupa el 90% de la altura del contenedor
+            Container(
+              height: containerHeight * 0.9,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: double.infinity,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 1.0,
+                  enlargeCenterPage: false,
+                  autoPlay: true,
+                  autoPlayCurve: Curves.easeInOut,
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                ),
+                items: product.imageList?.map((imageUrl) => _buildImageItem(imageUrl)).toList() ?? [],
               ),
-              items: product.imageList?.map((imageUrl) => _buildImageItem(imageUrl)).toList() ?? [],
             ),
-          ),
-          // Información del producto que ocupa el 10% de la altura
-          Expanded(
-            flex: 1,
-            child: Container(
+            // Información del producto que ocupa el 10% de la altura del contenedor
+            Container(
+              height: containerHeight * 0.1,
               width: double.infinity,
               color: Colors.grey.shade200,
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -137,9 +54,9 @@ class ProductItemLanding extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        );
+      },
     );
   }
 
@@ -149,90 +66,7 @@ class ProductItemLanding extends StatelessWidget {
       decoration: BoxDecoration(
         image: DecorationImage(
           image: NetworkImage(imageUrl),
-          fit: BoxFit.scaleDown,
-        ),
-      ),
-    );
-  }
-} */
-
-import 'package:awsshop/models/product.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/material.dart';
-
-class ProductItemLanding extends StatelessWidget {
-  final Product product;
-
-  const ProductItemLanding({super.key, required this.product});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          // Altura del contenedor de acuerdo con el tamaño del viewport
-          double containerHeight = constraints.maxHeight;
-
-          return Column(
-            children: [
-              // Carrusel de imágenes que ocupa el 90% de la altura del contenedor
-              Flexible(
-                flex: 9,
-                child: Container(
-                  height: containerHeight * 0.8, // Ajusta la altura
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      height: double.infinity,
-                      aspectRatio: 16 / 9,
-                      viewportFraction: 1.0,
-                      enlargeCenterPage: false,
-                      autoPlay: true,
-                      autoPlayCurve: Curves.easeInOut,
-                      autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                    ),
-                    items: product.imageList?.map((imageUrl) => _buildImageItem(imageUrl)).toList() ?? [],
-                  ),
-                ),
-              ),
-              // Información del producto que ocupa el 10% de la altura del contenedor
-              Flexible(
-                flex: 1,
-                child: Container(
-                  width: double.infinity,
-                  color: Colors.grey.shade200,
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.name ?? 'Nombre del producto',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        product.price != null
-                            ? '\$${product.price!.toStringAsFixed(2)}'
-                            : 'Sin precio',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildImageItem(String imageUrl) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.scaleDown,
+          fit: BoxFit.cover,
         ),
       ),
     );

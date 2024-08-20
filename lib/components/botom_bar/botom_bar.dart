8677381f6534570b5c4b3dfe3d94a689646c333c
Muhammad Hicho Haidor <mh.haidor@gmail.com>
2024-08-20 11:31:18 +0200

    fix onItemSelected botomBar

--- lib/components/botom_bar/botom_bar.dart
+++ lib/components/botom_bar/botom_bar.dart
@@ -7,7 +7,6 @@ class BotomBarNavigation extends StatefulWidget {
   final int selectedIndex;
   final Color backgroundColorBottomBar;
   final Color waterDropColorBottomBar;
-
   const BotomBarNavigation({
     super.key,
     required this.onItemSelected,
@@ -15,7 +14,6 @@ class BotomBarNavigation extends StatefulWidget {
     required this.backgroundColorBottomBar,
     required this.waterDropColorBottomBar,
   });
-
   @override
   State<BotomBarNavigation> createState() => _BotomBarNavigationState();
 }
@@ -25,23 +23,17 @@ class _BotomBarNavigationState extends State<BotomBarNavigation> {
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle(
-          //this color must be equal to the WaterDropNavBar backgroundColor
           systemNavigationBarColor: widget.backgroundColorBottomBar,
-          systemNavigationBarDividerColor: widget
-              .waterDropColorBottomBar, // Color del divisor de la barra de navegación
-          systemNavigationBarIconBrightness:
-              Brightness.light, // Brillo de los iconos en la barra de navegación
-          systemNavigationBarContrastEnforced:
-              true, // Enforce contrast on the navigation bar
-          statusBarColor: Colors.red, // Color de la barra de estado
-          statusBarBrightness:
-              Brightness.dark, // Brillo de la barra de estado (para iOS)
-          statusBarIconBrightness: Brightness
-              .light, // Brillo de los iconos de la barra de estado (para Android)
-          systemStatusBarContrastEnforced:
-              true, // Enforce contrast on the status bar
+          systemNavigationBarDividerColor: widget.waterDropColorBottomBar,
+          systemNavigationBarIconBrightness: Brightness.light,
+          systemNavigationBarContrastEnforced: true,
+          statusBarColor: Colors.red,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.light,
+          systemStatusBarContrastEnforced: true,
         ),
         child: WaterDropNavBar(
+          bottomPadding: 20,
           barItems: [
             BarItem(
               filledIcon: Icons.home_rounded,
@@ -52,80 +44,12 @@ class _BotomBarNavigationState extends State<BotomBarNavigation> {
               outlinedIcon: Icons.grid_view_outlined,
             ),
           ],
-          selectedIndex: 0,
-          onItemSelected: (index) {
-            // Manejar la selección del ítem
-          },
-          buttonPadding: 12.0, // Padding para los botones
+          selectedIndex: widget.selectedIndex,
+          onItemSelected: widget.onItemSelected,
           backgroundColor: widget.backgroundColorBottomBar,
           waterDropColor: widget.waterDropColorBottomBar,
-          iconSize: 30,
-          inactiveIconColor:
-              widget.waterDropColorBottomBar.withOpacity(0.5), // Color de icono inactivo
-        )); /*  */
+          iconSize: 35,
+          inactiveIconColor: widget.waterDropColorBottomBar.withOpacity(0.5),
+        ));
   }
-}
-
-class WaterDropNavBar extends StatelessWidget {
-  final List<BarItem> barItems;
-  final int selectedIndex;
-  final void Function(int) onItemSelected;
-  final double?
-      buttonPadding; // Agrega el parámetro de padding para los botones
-  final Color backgroundColor;
-  final Color waterDropColor;
-  final double iconSize;
-  final Color? inactiveIconColor;
-
-  const WaterDropNavBar({
-    required this.barItems,
-    required this.selectedIndex,
-    required this.onItemSelected,
-    this.buttonPadding, // Inicializa el parámetro de padding
-    this.backgroundColor = Colors.white,
-    this.waterDropColor = const Color(0xFF5B75F0),
-    this.iconSize = 28,
-    this.inactiveIconColor,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: backgroundColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: barItems.map((barItem) {
-          final isSelected = barItems.indexOf(barItem) == selectedIndex;
-          final color = isSelected
-              ? waterDropColor
-              : (inactiveIconColor ?? Colors.grey.withOpacity(0.6));
-          return Padding(
-            padding: EdgeInsets.all(
-                buttonPadding ?? 0.0), // Aplica el padding a los botones
-            child: GestureDetector(
-              onTap: () {
-                onItemSelected(barItems.indexOf(barItem));
-              },
-              child: Icon(
-                isSelected ? barItem.filledIcon : barItem.outlinedIcon,
-                size: iconSize,
-                color: color,
-              ),
-            ),
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
-
-class BarItem {
-  final IconData filledIcon;
-  final IconData outlinedIcon;
-
-  BarItem({
-    required this.filledIcon,
-    required this.outlinedIcon,
-  });
 }
